a0b859b64a5ca48e5828acddbfde7134d2b122db
Alexey Shcheglov <aleexxx2612@gmail.com>
2022-02-02 21:44:26 +0400

    Выполнено усложненное задание пятого урока

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-const num = 266219;
-const str = num.toString();
+'use strict';
 
-const arr = str.split('');
 
-let result = 1;
+const arr = ['25', '327', '411', '85', '222', '43', '121'];
 
-for (let i=0; i < arr.length; i++) {
-	result = result * Number(arr[i]);
+for (let i = 0; i < 7; i++) {
+	if(arr[i].startsWith(2) || arr[i].startsWith(4)) {
+		console.log(arr[i]);
+	}
 }
 
-result = result ** 3;
-
-result = result.toString().slice(0, 2);
 
 
+nextPrime:
+for (let i = 2; i <= 100; i++) { 
 
-console.log(result);
+  for (let j = 2; j < i; j++) { 
+    if (i % j == 0) continue nextPrime; 
+  }
+
+  console.log(`Делители числа ${i} : 1 и ${i}`); 
+}

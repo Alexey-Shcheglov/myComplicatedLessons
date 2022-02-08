db4fa234f52510d82df7199d961378843ea8f85f
Alexey Shcheglov <aleexxx2612@gmail.com>
2022-02-08 19:57:20 +0400

    Выполнено усложненное задание седьмого урока

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 'use strict';
 
-const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']; // создали массив
+const myWeek = document.getElementById('myWeek'); // получили элемент по ID
+const day = new Date();
+
+
+week.forEach(function(i) {
+	if (i == 'Суббота' || i == 'Воскресенье') {
+		if (week[day.getDay()] == i ) {
+			myWeek.innerHTML += `<b>${i.italics()}</b></br>`;
+		} else {
+			myWeek.innerHTML += `${i.italics()}</br>`;
+		}
+	} else if (week[day.getDay()] == i) {
+		myWeek.innerHTML += `<b>${i}</b></br>`;
+	} else {
+		myWeek.innerHTML += i + '</br>';
+	}
+})
+

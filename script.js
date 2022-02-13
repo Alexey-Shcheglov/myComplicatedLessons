5e43e177ea8ee5737683a4da4287b388852975e7
Alexey Shcheglov <aleexxx2612@gmail.com>
2022-02-14 00:01:27 +0400

    Выполнено усложненное задание 11 урока

--- script.js
+++ script.js
@@ -1,21 +1,76 @@
 'use strict';
 
-const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']; // создали массив
-const myWeek = document.getElementById('myWeek'); // получили элемент по ID
-const day = new Date();
+const showTime1 = document.getElementById('ShowTime1');
+const showTime2 = document.getElementById('ShowTime2');
+
+const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+const months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
 
 
-week.forEach(function(i) {
-	if (i == 'Суббота' || i == 'Воскресенье') {
-		if (week[day.getDay()] == i) {
-			myWeek.innerHTML += `<b>${i.italics()}</b></br>`;
-		} else {
-			myWeek.innerHTML += `${i.italics()}</br>`;
-		}
-	} else if (week[day.getDay()] == i) {
-		myWeek.innerHTML += `<b>${i}</b></br>`;
+const addZerro = function (num) {
+	return num > 10 ? num : '0' + num;
+};
+
+const declinationMonths = function (str) {
+	if (str === "Март" || str === "Август") {
+		return str + "а";
 	} else {
-		myWeek.innerHTML += i + '</br>';
+		return str.slice(0, -1) + "я";
 	}
-});
+};
+
+const declinationHours = function (num) {
+	if (num == 1 || num == 21) {
+		return "час";
+	} else if (num >= 2 && num <= 4 || num >= 22 && num <= 24) {
+		return "часа";
+	} else {
+		return "часов";
+	}
+};
+
+const declinatinMinutes = function (num) {
+	if (num % 10 === 1) {
+		return 'минута';
+	} else if (num % 10 === 2 ||num % 10 === 3 ||num % 10 ===4 ) {
+		return 'минуты';
+	} else {
+		return 'минут';
+	}
+};
+
+const declinationSeconds = function (num) {
+	if (num % 10 === 1) {
+		return 'секунда';
+	} else if (num % 10 === 2 ||num % 10 === 3 ||num % 10 === 4) {
+		return 'секунды';
+	} else {
+		return 'секунд';
+	}
+};
+
+
+const getDayWeek = function (date) {
+	return days[date.getDay()];
+};
+
+const getMonth = function (date) {
+	return months[date.getMonth()];
+};
+
+const app = function () {
+	setInterval(function() {
+		const date = new Date();
+		showTime1.innerText = 'Сегодня ' + getDayWeek(date) + ', ' + date.getDate() + ' ' + declinationMonths(getMonth(date)) + ' ' + date.getFullYear() + ' года, '
+			+ addZerro(date.getHours()) + ' ' + declinationHours(date.getHours()) + ' ' + addZerro(date.getMinutes()) + ' ' + declinatinMinutes(date.getMinutes())
+			+ ' ' + addZerro(date.getSeconds()) + ' ' + declinationSeconds(date.getSeconds()); 
+		}, 1000);
+		showTime2.innerText = date.toLocaleDateString('ru') + ' - ' + date.toLocaleTimeString('ru');
+		
+};
+
+
+app();
+
+
 

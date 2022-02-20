9d3b9dc5d0c48711087285f686e231bf359d7eb4
Alexey Shcheglov <aleexxx2612@gmail.com>
2022-02-20 18:40:18 +0400

    lesson15 completed

--- script.js
+++ script.js
@@ -1,53 +1,32 @@
 'use strict';
+//  основное задание
 
-const showTime1 = document.getElementById('ShowTime1');
-const showTime2 = document.getElementById('ShowTime2');
+const DomElement = function (selector, height, width, bg, fontSize) {
+	this.selector = selector;
+	this.height = height + 'px';
+	this.width = width + 'px';
+	this.bg = bg;
+	this.fontSize = fontSize + 'px';
+	this.createElement = () => {
+		let elem;
+		if (this.selector.trim()[0] === ".") {
+			 elem = document.createElement('div');
+			 elem.classList.add(`${selector.slice(1)}`);
+		} else if (this.selector.trim()[0] === "#") {
+			 elem = document.createElement('p');
+			 elem.setAttribute("id", `${selector.slice(1)}`);
+		}
 
-const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-const months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+		document.body.append(elem);
+		elem.innerText = 'Создан элемент, причину пояснять не буду';
+		elem.style.cssText=`height: ${height  + 'px'};
+			width: ${width  + 'px'};
+			background-color: ${bg};
+			font-size : ${fontSize  + 'px'}; `;
+  };
+ };
+
+ let elem = new DomElement('.block', '200', '200', 'blue', '22');
+ elem.createElement();
 
 
-const addZerro = function (num) {
-	return num > 10 ? num : '0' + num;
-};
-
-const declinationMonths = function (str) {
-	if (str === "Март" || str === "Август") {
-		return str + "а";
-	} else {
-		return str.slice(0, -1) + "я";
-	}
-};
-
-function decOfTime(number, type) {
-	const decCases = [2, 0, 1, 1, 1, 2];
-	const types = {
-		hours: ['час', 'часа', 'часов'],
-		minutes: ['минута', 'минуты', 'минут'],
-		seconds: ['секунда', 'секунды', 'секунд']
-	};
-  
-    return types[type][number % 100 > 4 && number % 100 < 20 ? 2 : decCases[Math.min(number % 10, 5)]];
-}
-
-const getDayWeek = function (date) {
-	return days[date.getDay()];
-};
-
-const getMonth = function (date) {
-	return months[date.getMonth()];
-};
-
-const app = function () {
-	setInterval(function () {
-		const date = new Date();
-		showTime1.innerText = 'Сегодня ' + getDayWeek(date) + ', ' + date.getDate() + ' ' + declinationMonths(getMonth(date)) + ' ' + date.getFullYear() + ' года, ' +
-			addZerro(date.getHours()) + ' ' + decOfTime(date.getHours(), 'hours') + ' ' + addZerro(date.getMinutes()) + ' ' + decOfTime(date.getMinutes(), 'minutes') +
-			' ' + addZerro(date.getSeconds()) + ' ' + decOfTime(date.getSeconds(), 'seconds');
-		showTime2.innerText = date.toLocaleDateString('ru') + ' - ' + date.toLocaleTimeString('ru');
-
-	}, 1000);
-
-};
-
-app();
